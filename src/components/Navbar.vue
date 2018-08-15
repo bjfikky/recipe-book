b06ebb6235c15013ce2117b4c92fc50c45f9ca9e
Benjamin Orimoloye <bjfikky@yahoo.com>
2018-08-14 23:52:50 -0400

    Added the navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="navbar">
+        <div class="nav-wrapper">
+            <span><router-link to="/" class="brand-logo">Recipe Book</router-link></span>
+            <button class="btn-floating btn-large halfway-fab waves-effect waves-light teal">
+                <i class="material-icons">add</i>
+            </button>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar',
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navbar {
+    padding: 0 20px;
+}
+</style>
